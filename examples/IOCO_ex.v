688cea731167e6ca766f29600ccbfe77c5a5073a
Beatriz Alves <babs@cin.ufpe.br>
2022-04-07 07:07:49 -0700

    prove i1 not ioco s3

--- examples/IOCO_ex.v
+++ examples/IOCO_ex.v
@@ -1,5 +1,8 @@
-Require Export IOCO.
-Require Export IOTS.
+Require Import IOCO.
+Require Import IOTS.
+Require Import LTS.
+Require Import list_helper.
+Require Import BE_ltacs.
 Require Import LTS_ex.
 Require Export IOTS_ex.
 Require Import Coq.Lists.List.
@@ -67,4 +70,132 @@ Example test_fout_8 :
   ) fig4_k3 = [].
 Proof.
   reflexivity.
+Qed.
+
+
+Example i1_ioco_s1 : ind_ioco imp_i1 spec_s1_IOLTS.
+Proof. Admitted.
+
+(*
+  unfold ind_ioco. intros Qi Qs t out_i out_s H H1 H2 H3 H4.
+  apply ind_out_equiv_f_out in H3. simpl in H3.
+  apply ind_out_equiv_f_out in H4. simpl in H4.
+  unfold incl. intros l H5. apply H3 in H5. apply H4.
+  clear H1.
+  assert (H1 : Qi [=] (f_after_IOLTS t (create_s_IOLTS (embedded_iolts imp_i1)))).
+  { admit. }
+  clear H2.
+  assert (H2 : Qs [=] (f_after_IOLTS t (create_s_IOLTS spec_s1_IOLTS))).
+  { admit. }
+  clear H3 H4.
+  induction H. simpl in H. induction t.
+  - simpl in H. unfold f_after_IOLTS in H2. unfold f_after_single_state_IOLTS in H2.
+    simpl in H2. unfold LTS_functions.all_reachable_by_tau in H2. simpl in H2.  admit.
+  - 
+  unfold incl. intros l' H'.
+  unfold ind_out in H3. specialize (H3 l'). destruct H3 as [H3_1 H3_2].
+  unfold ind_out in H4. specialize (H4 l'). destruct H4 as [H4_1 H4_2].
+  apply H3_1 in H'. destruct H' as [s'' [so [H'_1 [H'_2 H'_3]]]].
+  inversion H'_2; subst.
+  + expand_In H'_3; subst. expand_In H3; subst.
+    * unfold ind_s_after_LTS in H1. unfold ind_s_after in H1. simpl in *.
+      unfold ind_s_after_LTS in H2. unfold ind_s_after in H2. simpl in H2.
+      apply H1 in H'_1. inversion H'_1; subst.
+      * simpl in H0. unfold ind_s_traces_LTS in H.
+        unfold ind_s_traces in H. destruct H as [s'' H]. simpl in H. inversion H; subst.
+        simpl in *. inversion H5; subst.
+        -- apply H2 in H. apply H4_2 in H. destruct H as [so' [H_1 H_2]].
+           inversion H_1; subst.
+           ++ apply H_2. elem_in_list.
+           ++ destruct H. elem_in_list.
+        -- inversion H6; subst. expand_In H8.
+      * simpl in *. inversion H0; subst. inversion H6; subst.
+        -- inversion H7; subst. expand_In H11; subst.
+    unfold ind_out in H4. specialize (H4 l). destruct H4 as [H4_1 H4_2].
+    
+  
+  
+   inversion H0; subst.
+    + intros H1 H2 H3 H4. unfold ind_s_after_LTS in H2. unfold ind_s_after in H2.
+      simpl in H2. apply H2 in H. 
+      apply H4_2 in H. destruct H as [so' [H_1 H_2]]. inversion H_1; subst.
+      * apply H_2. apply H3_1 in H''.
+    intros H'. unfold ind_s_after_LTS in H'. unfold ind_s_after in H'.
+    simpl in *. intros H1 H2 H3.  
+    unfold ind_out in H2. specialize (H2 l). destruct H2 as [H2_1 H2_2].
+    unfold ind_out in H3. specialize (H3 l). destruct H3 as [H3_1 H3_2].
+    apply H2_1 in H''.
+    destruct H'' as [s [so' [H''_1 [H''_2 H''_3]]]]. inversion H''_2; subst.
+    + expand_In H''_3; subst. unfold ind_s_after_LTS in H1. unfold ind_s_after in H1.
+      simpl in *. 
+      *
+Admitted.
+*)
+
+Example i1_not_ioco_s3 : ~ (ind_ioco imp_i1 spec_s3_IOLTS).
+Proof.
+  unfold ind_ioco. unfold not. intros H. simpl in H.
+  specialize (H [1] [4] [s_event "b"] [delta] [s_event "y"] ).
+  assert ( H': ~ incl [delta] [s_event "y"]).
+  { unfold incl. intros H''. specialize (H'' delta). destruct H'';
+    try inversion H0. left. reflexivity.
+  }
+  apply H'. apply H.
+  - unfold ind_s_traces_LTS. unfold ind_s_traces. exists 4. simpl.
+    apply s_seq_reachability_r2 with (si := 4).
+    + apply one_step_reachability_r1 with (s1 := 1) (s2 := 4); try apply empty_reachability_r1.
+      apply transition_r1. elem_in_list.
+    + apply s_seq_reachability_r1. apply empty_reachability_r1.
+  - unfold ind_s_after_LTS. unfold ind_s_after. intros a. split.
+    + simpl. intros H''. inversion H''; subst. simpl in *. inversion H3; subst.
+      inversion H0; subst.
+      * inversion H1; subst. expand_In H7. subst. inversion H2; subst.
+        -- inversion H6; subst. simpl in H4. inversion H4; subst.
+           ++ left. reflexivity.
+           ++ inversion H5; subst. expand_In H9.
+        -- inversion H4; subst. expand_In H8.
+      * inversion H4; subst. expand_In H7.
+    + intros H''. expand_In H''. simpl. apply s_seq_reachability_r2 with (si := 1).
+      * apply one_step_reachability_r1 with (s1 := 1) (s2 := 1); try apply empty_reachability_r1.
+        apply transition_r1. elem_in_list.
+      * apply s_seq_reachability_r1. apply empty_reachability_r1.
+  - unfold ind_s_after_LTS. unfold ind_s_after. intros a. split.
+    + simpl. intros H''. inversion H''; subst. simpl in *. inversion H3; subst.
+      inversion H0; subst.
+      * inversion H1; subst. expand_In H7. subst. inversion H2; subst.
+        -- inversion H6; subst. simpl in H4. inversion H4; subst.
+           ++ left. reflexivity.
+           ++ inversion H5; subst. expand_In H9.
+        -- inversion H4; subst. expand_In H8.
+      * inversion H4; subst. expand_In H7.
+    + intros H''. expand_In H''. simpl. apply s_seq_reachability_r2 with (si := 4).
+      * apply one_step_reachability_r1 with (s1 := 1) (s2 := 4); try apply empty_reachability_r1.
+        apply transition_r1. elem_in_list.
+      * apply s_seq_reachability_r1. apply empty_reachability_r1.
+  - unfold ind_out. intros l. split.
+    + intros H''. expand_In H''. exists 1. exists [delta].
+      repeat split; try(apply out_one_state_r1); elem_in_list.
+    + intros s H''. expand_In H''. exists [delta]. split.
+      * try(apply out_one_state_r1); elem_in_list.
+      * intros o H'''. expand_In H'''. elem_in_list.
+  - unfold ind_out. intros l. split.
+    + intros H''. expand_In H''. exists 4. exists [s_event "y"].
+      repeat split; try elem_in_list. apply out_one_state_r2.
+      * intros H'''; expand_In H'''.
+      * intros H'''; expand_In H'''.
+      * intros l'. split.
+        -- intros H'''. expand_In H'''. exists 5. split; try (elem_in_list).
+           apply transition_r1. elem_in_list.
+        -- intros H'''. destruct H''' as [ s' [ H'''_1 H'''_2]]. inversion H'''_2; subst.
+           expand_In H'''_1; subst; expand_In H3. elem_in_list.
+    + intros s H''. expand_In H''. exists [s_event "y"]. split.
+      * apply out_one_state_r2.
+        -- intros H'''; expand_In H'''.
+        -- intros H'''; expand_In H'''.
+        -- intros l'. split.
+           ++ intros H'''. expand_In H'''. exists 5. split; try (elem_in_list).
+           apply transition_r1. elem_in_list.
+           ++ intros H'''. destruct H''' as [ s' [ H'''_1 H'''_2]]. inversion H'''_2; subst.
+           expand_In H'''_1; subst; expand_In H3. elem_in_list.
+      * intros l' H'''. expand_In H'''. elem_in_list.
 Qed.
--- examples/IOTS_ex.v
+++ examples/IOTS_ex.v
@@ -1,3 +1,4 @@
+Require Import IOCO.
 Require Import BE_ltacs.
 Require Import String.
 Require Import Coq.Lists.List.
@@ -147,6 +148,117 @@ Proof.
       * apply one_step_reachability_r1 with (s1 := 3) (s2 := 3);
         try (apply empty_reachability_r1). apply transition_r1. elem_in_list.
       * apply seq_reachability_r1. apply empty_reachability_r1.
+Defined.
+
+Definition spec_s1_LTS : LTS.
+Proof.
+  solve_LTS_rules
+    [1;2;3]
+    ["a";"x"]
+    [(1, event "a", 2);(2, event "x", 3)]
+    1.
+Defined.
+
+Definition spec_s1_SC_LTS : SC_LTS.
+Proof.
+ apply (mkSC_LTS spec_s1_LTS).
+ unfold strongly_converging. intros s t H. destruct t.
+  - destruct H. unfold not in H. exfalso. apply H. reflexivity.
+  - unfold all_labels_tau in H. destruct H as [_ [H _]].
+    unfold not. intros H'. inversion H'.
+    + subst. proof_absurd_transition H5.
+    + subst. explore_transition H3.
+Defined.
+
+Definition spec_s1_IOLTS : IOLTS.
+Proof.
+  solve_IOLTS_rules
+    spec_s1_SC_LTS
+    ["a"]
+    ["x"].
+Defined.
+
+(*
+
+Theorem i1_ioco_s1 : ind_ioco imp_i1 spec_s1_IOLTS.
+Proof.
+  unfold ind_ioco. simpl. intros Qi Qs t out_i out_s H.
+  unfold ind_s_traces_LTS in H. unfold ind_s_traces in H. destruct H as [s' H].
+  simpl in H. inversion H; subst.
+  - clear. intros H1 H2. unfold ind_s_after_LTS in H2. simpl in H2. unfold ind_s_after in H2.
+    intros H3 H4. unfold ind_out in H4. intros l H'.
+    assert (H'': forall s, In s Qs -> In l out_s).
+    { intros s H. remember H as H_new. clear HeqH_new. apply H2 in H.
+      inversion H. subst. inversion H0.
+      - subst. simpl in *. specialize (H4 l). destruct H4 as [_ H4].
+        apply H4 in H_new. destruct H_new as [so' [H_new1 H_new2]].
+        inversion H_new1; subst.
+        +
+    
+    }
+    apply H''.
+
+
+
+
+
+  unfold ind_ioco. simpl. intros Qi Qs t out_i out_s H1 H2 H3 H4 H5.
+  unfold incl. intros l. intros H6. destruct l.
+  - admit.
+  - unfold ind_out in H4. specialize (H4 delta).
+    destruct H4 as [H4 _]. apply H4 in H6. clear H4. destruct H6 as [s [so' H6]].
+    destruct H6 as [H6_1 [H6_2 H6_3]]. inversion H6_2; subst.
+    + unfold ind_out in H5. specialize (H5 delta). destruct H5 as [_ H5].
+      unfold ind_s_after_LTS in H3. unfold ind_s_after in H3. simpl in H3.
+  unfold ind_out in H4. specialize (H4 l).
+  destruct H4 as [H4 _]. apply H4 in H6. clear H4. destruct H6 as [s [so' H6]].
+  destruct H6 as [H6_1 [H6_2 H6_3]]. inversion H6_2; subst.
+  - admit.
+  - simpl in H4. unfold ind_out in H5. specialize (H5 l). destruct H5 as [_ H5].
+    unfold ind_s_after_LTS in H3. unfold ind_s_after in H3. simpl in H3.
+  
+  
+  
+  
+  unfold ind_s_traces_LTS in H1.
+  unfold ind_s_traces in H1. destruct H1 as [s' H1].
+  simpl in H1. inversion H1; subst.
+  - unfold ind_s_after_LTS in H2. unfold ind_s_after in H2. simpl in H2.
+    unfold ind_s_after_LTS in H3. unfold ind_s_after in H3. simpl in H3.
+    unfold ind_out in H4. apply H4 in H6. destruct H6 as [s [so' H6]].
+    destruct H6 as [ [H6_1 [H6_2 H6_3]] _]. clear H4. inversion H6_2. 
+    + subst. simpl in H0.
+  unfold ind_s_after_LTS in H2. unfold ind_s_after in H2. specialize (H2 x).
+   unfold ind_s_after_LTS in H3. unfold ind_s_after in H3. specialize (H3 x).
+   apply H3 in H1. unfold ind_out in H5. specialize (H5 a). 
+   -
+*)
+Definition spec_s3_LTS : LTS.
+Proof.
+  solve_LTS_rules
+    [1;2;3;4;5]
+    ["a";"b";"x";"y"]
+    [(1, event "a", 2);(2, event "x", 3);(1, event "b", 4);(4, event "y", 5)]
+    1.
+Defined.
+
+Definition spec_s3_SC_LTS : SC_LTS.
+Proof.
+ apply (mkSC_LTS spec_s3_LTS).
+ unfold strongly_converging. intros s t H. destruct t.
+  - destruct H. unfold not in H. exfalso. apply H. reflexivity.
+  - unfold all_labels_tau in H. destruct H as [_ [H _]].
+    unfold not. intros H'. inversion H'.
+    + subst. proof_absurd_transition H5.
+    + subst. explore_transition H3.
+Defined.
+
+Definition spec_s3_IOLTS : IOLTS.
+Proof.
+  solve_IOLTS_rules
+    spec_s3_SC_LTS
+    ["a";"b"]
+    ["x";"y"].
 Defined.
 
 Local Close Scope string.
