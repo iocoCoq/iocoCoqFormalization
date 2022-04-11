185eac3c79243cf6f0c9c473320ccea137e14d26
Gustavo Carvalho <ghpc@cin.ufpe.br>
2022-04-11 16:11:53 -0300

    More steps concerning the proof i1_ioco_s1

--- examples/IOCO_ex.v
+++ examples/IOCO_ex.v
@@ -110,6 +110,71 @@ Proof.
   - admit.
 Admitted.
 
+(* TODO: adjust to Q1 [=] Q2 *)
+Lemma ind_s_after_IOLTS_deterministic :
+  forall (t : list s_label) (s : s_IOLTS) (Q1 Q2 : set state),
+    ind_s_after_IOLTS t Q1 s ->
+    ind_s_after_IOLTS t Q2 s ->
+    Q1 = Q2.
+Proof.
+Admitted.
+
+Lemma i1_states_after_empty_aux :
+  ind_s_after_IOLTS [] [1] (create_s_IOLTS (embedded_iolts imp_i1)).
+Proof.
+  unfold ind_s_after_IOLTS. simpl. unfold ind_s_after. intros. split.
+  - intros H. inversion H. subst. simpl in H0.
+    inversion H0 ; subst.
+    -- simpl. left. reflexivity.
+    -- inversion H1. subst. simpl in H3.
+       repeat (destruct H3 ; inversion H3).
+  - intros H. simpl in H. destruct H as [H | H].
+    -- subst. apply s_seq_reachability_r1. simpl.
+       apply empty_reachability_r1.
+    -- contradiction.
+Qed.
+
+Lemma i1_states_after_empty :
+  forall (Q : set state),
+    ind_s_after_IOLTS [] Q (create_s_IOLTS (embedded_iolts imp_i1))
+    -> Q = [1].
+Proof.
+  intros Q. intros H1. simpl in H1.
+  pose proof (i1_states_after_empty_aux) as H2.
+  simpl in H2.
+  pose proof (
+    ind_s_after_IOLTS_deterministic
+      []
+      (create_s_IOLTS imp_i1_IOLTS)
+      Q
+      [1]
+      H1
+      H2
+  ) as H3.
+  apply H3. 
+Qed.
+
+Lemma s1_states_after_empty :
+  forall (Q : set state),
+    ind_s_after_IOLTS [] Q (create_s_IOLTS spec_s1_IOLTS)
+    -> Q = [1].
+Proof.
+Admitted.
+
+Lemma i1_out_empty :
+  forall (out : set s_label),
+    ind_out [1] out (create_s_IOLTS (embedded_iolts imp_i1))
+    -> out = [delta].
+Proof.
+Admitted.
+
+Lemma s1_out_empty :
+  forall (out : set s_label),
+    ind_out [1] out (create_s_IOLTS spec_s1_IOLTS)
+    -> out = [delta].
+Proof.
+Admitted.
+
 Example i1_ioco_s1 : ind_ioco imp_i1 spec_s1_IOLTS.
 Proof.
   unfold ind_ioco. intros Qi Qs t out_i out_s H.
@@ -120,8 +185,15 @@ Proof.
   generalize dependent H.
   rewrite s1_traces. intros H.
   intros out_s out_i Qs Qi.
-  destruct H as [H | [H | H]] ; rewrite H.
-  - admit.
+  destruct H as [H | [H | H]] ; rewrite H ; intros H1 H2 H3 H4.
+  - apply i1_states_after_empty in H1.
+    apply s1_states_after_empty in H2.
+    subst.
+    apply i1_out_empty in H3.
+    apply s1_out_empty in H4.
+    subst. unfold incl.
+    intros a H.
+    apply H.
   - admit.
   - admit.
 Admitted.
